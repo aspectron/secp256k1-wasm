d1e0a53d9c15d0df2565a4bbed4f7bcda620e2fb
Anton Yemelyanov <anton.yemelyanov@gmail.com>
2021-01-16 03:26:28 -0500

    addressing async wasm loading issues

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
-const secp256k1 = require('./http/secp256k1');
-module.exports = secp256k1;
+// ensure only a single instance of secp256k1 exists
+// in the node environment as multiple instances may
+// be problematic when loading wasm asynchronously
+if(!global.__secp256k1__)
+	global.__secp256k1__ = require('./http/secp256k1');
+module.exports = global.__secp256k1__;
